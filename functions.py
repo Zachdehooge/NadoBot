2401cd6eb8fe7b83faf7fabb5e90c842bf3a7d7c
Arch <arch1010@icloud.com>
2024-07-10 17:30:20 -0500

    Add Nadocast scraper and image sending functionality

--- functions.py
+++ functions.py
@@ -1,11 +1,71 @@
 import aiohttp
 import os
 from datetime import datetime, timedelta, timezone
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
-#discontinued?
+
+# discontinued?
 async def getUTCTime() -> datetime:
-    dt = datetime.now(timezone.utc) 
-  
-    utc_time = dt.replace(tzinfo=timezone.utc) 
+    dt = datetime.now(timezone.utc)
+
+    utc_time = dt.replace(tzinfo=timezone.utc)
 
     return utc_time
+
+
+async def getNadoCastData(time: datetime) -> list[str]:
+
+    # Get specific data from the datetime object
+    month = time.strftime("%m")
+    day = time.strftime("%d")
+    year = time.strftime("20%y")
+    timeNow = time.strftime("%H")
+    timeNowInt = int(timeNow)
+
+    # Since the data is only available at 0Z, 12Z, 18Z, we need to round the time to the nearest available time
+    if timeNowInt < 13:
+        timeNow = 0
+    elif 13 <= timeNowInt < 18:
+        timeNow = 12
+    elif 18 <= timeNowInt < 24:
+        timeNow = 18
+
+    # URL Structure
+    url = "{4}{2}{1}/{2}{1}{0}/t{3}z/".format(
+        day, month, year, timeNow, os.getenv("URL")
+    )
+
+    # Create a list so we can use it back in main.py
+    file_list = []
+
+    # Folder Structure, and create the folder if it doesn't exist, but if it does, return the already downloaded folders
+    folder_location = r"Nadocast\\{1}_{0}_{2}_{3}z".format(day, month, year, timeNow)
+    if os.path.exists(folder_location):
+        for file in os.listdir(folder_location):
+            file_list.append(os.path.join(folder_location, file))
+        file_list.sort()
+        return file_list
+
+    if not os.path.exists(folder_location):
+        os.makedirs(folder_location)
+
+    # Get the html text from the url
+    response = requests.get(url)
+
+    # Parse the html text
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Download the images
+    for link in soup.select("a[href$='.png']"):
+        # Name the png files using the last portion of each link which are unique in this case
+        filename = os.path.join(folder_location, link["href"].split("/")[-1])
+
+        file_list.append(filename)
+
+        with open(filename, "wb") as f:
+            f.write(requests.get(urljoin(url, link["href"])).content)
+
+    file_list.sort()
+    return file_list
--- main.py
+++ main.py
@@ -5,23 +5,88 @@ import discord
 import time
 import os
 
-#Retrive token from .env
+# Retrive token from .env
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN: str = os.getenv("TOKEN")
 
-#configure bot
+# Configure Bot
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Create bot client
 client = commands.Bot(command_prefix="$", intents=intents)
 
-#commands
+
+# Events
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print(f"We have logged in as {client.user}")
 
-@client.command(name="getUTC")
-async def getUnixTime(ctx):
-    await ctx.send(f'<t:{int(time.time())}:f>')
 
-client.run(TOKEN)
+# Commands
+@client.command(name="getUTC")
+async def getUTC(ctx) -> None:
+    utc_time = await getUTCTime()
+    await ctx.send(utc_time.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+@client.command(name="test")
+async def test(ctx, *args) -> None:
+    await ctx.send(args[0])
+
+
+@client.command(name="fetch")
+async def fetch(ctx, *args) -> None:
+    await ctx.send("Fetching...")
+
+    allowed_params = ["sig", "tor", "wind", "hail"]
+
+    # Lets check the args to make sure we should do this request.
+    try:
+        allowed_params.index(args[0])
+
+        if args[0] == "sig" and args[1] != None:
+            allowed_params.index(args[1])
+
+    except:
+        return await ctx.send(
+            "Incorrect params! Example of proper commands: \n$fetch sig tor \n$fetch tor"
+        )
+
+    # Fetch data, get our list of images
+    result = await getNadoCastData(await getUTCTime())
+    # print(result)
+    # Send the images
+    files = []
+    # debug = []
+    # print(args)
+
+    for file in result:
+        if (
+            args[0] != "sig"
+            and args[0] != "None"
+            and args[0] in file
+            and "sig" not in file
+        ):
+            files.append(discord.File(file))
+            # debug.append(file)
+            continue
+        if args[0] == "sig" and args[1] != "None" and f"{args[0]}_{args[1]}" in file:
+            files.append(discord.File(file))
+            # debug.append(file)
+            continue
+
+    if len(files) == 0:
+        return await ctx.send(
+            "It appears Nadocast has not put out the new images for this time range! Please try again in a minute."
+        )
+    # debug.sort()
+    # await ctx.send(debug)
+    await ctx.send(files=files)
+
+
+if type(TOKEN) == type(None):
+    print("Please follow the readme to setup the bot!")
+
+if __name__ == "__main__":
+    client.run(TOKEN)
