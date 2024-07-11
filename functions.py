7cac1f6594d2eef493a6dcc5cee8fadeeca3b96e
Arch <arch1010@icloud.com>
2024-07-11 15:18:09 -0500

    (Update): Logs, Fallback, User Info for Fallback

--- functions.py
+++ functions.py
@@ -43,17 +43,62 @@ async def getNadoCastData(time: datetime) -> list[str]:
 
     # Folder Structure, and create the folder if it doesn't exist, but if it does, return the already downloaded images
     folder_location = r"Nadocast\\{1}_{0}_{2}_{3}z".format(day, month, year, timeNow)
-    if os.path.exists(folder_location):
+
+    # Get the html text from the url
+
+    await log(f"Fetching images for {timeNow}z (from {url})")
+
+    response = requests.get(url)
+
+    # Check if the response is valid
+    if response.status_code != 200:
+        # Create fallback
+
+        time = time - timedelta(hours=6)
+
+        month = time.strftime("%m")
+        day = time.strftime("%d")
+        year = time.strftime("20%y")
+        timeNow = time.strftime("%H")
+        timeNowInt = int(timeNow)
+
+        if timeNowInt < 13:
+            timeNow = 0
+        elif 13 <= timeNowInt < 18:
+            timeNow = 12
+        elif 18 <= timeNowInt < 24:
+            timeNow = 18
+
+        url = "{4}{2}{1}/{2}{1}{0}/t{3}z/".format(
+            day, month, year, timeNow, os.getenv("URL")
+        )
+
+        await log(
+            f"Previous URL was 404, had to fallback, fetching images for {timeNow}z (from {url})"
+        )
+
+        # Since the data is only available at 0Z, 12Z, 18Z, we need to round the time to the nearest available time
+
+        folder_location = r"Nadocast\\{1}_{0}_{2}_{3}z".format(
+            day, month, year, timeNow
+        )
+
+    
+    if os.path.exists(folder_location) and os.listdir(folder_location) != []:
         for file in os.listdir(folder_location):
             file_list.append(os.path.join(folder_location, file))
         file_list.sort()
+        # await log(f"Images fetched for {timeNow}z: {file_list}")
+        await log(
+            f"Images for {timeNow}z have already been downloaded, returning them instead of downloading new ones."
+        )
         return file_list
+    
+    response = requests.get(url)
 
     if not os.path.exists(folder_location):
+        await log(f"Had to create a new folder ({folder_location})")
         os.makedirs(folder_location)
-
-    # Get the html text from the url
-    response = requests.get(url)
 
     # Parse the html text
     soup = BeautifulSoup(response.text, "html.parser")
@@ -68,12 +113,29 @@ async def getNadoCastData(time: datetime) -> list[str]:
         with open(filename, "wb") as f:
             f.write(requests.get(urljoin(url, link["href"])).content)
 
+    sepeartor = " ,"
+    text = sepeartor.join(file_list)
+    if text == "":
+        text = "No images found"
+    # await log(f"Images fetched for {timeNow}z: {file_list}")
+    await log(f"New images have been downloaded for {timeNow}z, returning them.")
+
     file_list.sort()
+
+    # OLD CODE, KEPT incase this does get triggered, meaning the list is somehow empty when that should not be possible.
     if len(file_list) == 0:
-        os.rmdir(folder_location)
-        with open("error.log", "a") as f:
+        with open("logs/error.log", "a") as f:
             f.write(
-                f"{datetime.now()} - No images found for {folder_location}. Command locked for 1 minute. Folder deleted. \n"
+                f"{datetime.now()} - No images found for {folder_location}. Command locked for 1 minute. [THIS SHOULD NOT TRIGGER]\n"
             )
         return None
     return file_list
+
+
+async def log(*params):
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+    with open("logs/general.log", "a") as f:
+        seperator = " "
+        text = seperator.join(params)
+        f.write(f"{datetime.now()} - {text} \n")
