5732e56da2dde7ea98f50bf2904620e3feb016ab
zachdehooge <Zacharydehooge@gmail.com>
2026-01-08 11:08:02 -0500

    Radar Links

--- features/alerts/alerts.py
+++ features/alerts/alerts.py
@@ -1,1 +1,13 @@
 # A file to house the logic for the NWS alerts
+
+# Huntsville Radar: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=hunrad&java=script&mode=realtime&frames=10&interval=5
+
+# Iowa Radar: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=comprad&java=script&mode=realtime&frames=10&interval=5
+
+# North Illinois: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=lotrad&java=script&mode=realtime&frames=10&interval=5
+
+# South Dakota: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=sdrad&java=script&mode=realtime&frames=10&interval=5
+
+# Wichita KS: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=ictrad&java=script&mode=realtime&frames=10&interval=5
+
+# United States: https://mesonet.agron.iastate.edu/current/mcview.phtml?prod=usrad&java=script&mode=realtime&frames=10&interval=5
